de32321df997f2958ec5d7ab32a239cf948f6a1c
GOHSGameDesign4 <GOHSGameDesign4@gmail.com>
2022-02-17 18:08:29 -0800

    added talking to test

--- test 2/Assets/Test.cs
+++ test 2/Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("I'm a talking circle!");
     }
 
     // Update is called once per frame
